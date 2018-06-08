9c5e7c12ddd4c670e5baee18308558330a5d0a84
YihengJian <YihengJian@yahoo.com>
2018-06-08 16:08:48 +0800

    generate bitstream
    change 16'0010xxxxxxxx into 16'b00000010xxxxxxxx

--- Sonic/Sonic.runs/synth_1/Top.tcl
+++ Sonic/Sonic.runs/synth_1/Top.tcl
@@ -16,7 +16,7 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param synth.incrementalSynthesisCache C:/Users/YihengJ/Documents/GitHub/FPGA-Project/Sonic/.Xil/Vivado-6924-DESKTOP-R99TAMK/incrSyn
+set_param synth.incrementalSynthesisCache C:/Xilinx/Vivado/2017.4/bin/unwrapped/win64.o/.Xil/Vivado-13184-DESKTOP-R99TAMK/incrSyn
 set_msg_config -id {Synth 8-256} -limit 10000
 set_msg_config -id {Synth 8-638} -limit 10000
 create_project -in_memory -part xc7a35ticsg324-1L
