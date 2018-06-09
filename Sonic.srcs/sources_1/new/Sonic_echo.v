b0bdfe95342f7a24962003f60a09a86011beef7b
YihengJian <YihengJian@yahoo.com>
2018-06-09 19:22:15 +0800

    return to display in HEX
    prove that the function of overflow is alright

--- Sonic.srcs/sources_1/new/Sonic_echo.v
+++ Sonic.srcs/sources_1/new/Sonic_echo.v
@@ -2,19 +2,12 @@
 module Sonic_echo(
     input clk, 
     input echo,
-    output [15:0] text,
-    output [15:0] text_out
+    output [15:0] text
 );
-
-reg [3:0] sec_0 = 4'b0;
-reg [3:0] sec_1 = 4'b0;
-reg [3:0] sec_2 = 4'b0;
-reg [3:0] sec_3 = 4'b0;
 
 reg [15:0] count = 16'b0;
 reg [15:0] distance = 16'b0;
 reg [15:0] distance_count = 16'b0;
-reg [15:0] distance_display = 16'b0;
 reg clk_out = 1'b0;
 
 reg [1:0] state = 2'b0;
@@ -48,27 +41,6 @@ case (state)
   state1:
   begin
     if (echo) begin
-      if(sec_0 < 9)
-        sec_0 <= sec_0 + 1'b1;
-      else begin
-        sec_0 <= 4'b0;
-        if (sec_1 < 9) begin
-          sec_1 <= sec_1 + 1'b1;
-        end else begin
-          sec_1 <= 4'b0;
-          if (sec_2 < 9) begin
-            sec_2 <= sec_2 + 1'b1;
-          end else begin
-            sec_2 <= 4'b0;
-            if(sec_3 < 9) begin
-                sec_3 <= sec_3 + 1;
-            end else begin
-                sec_3 <= 4'b0;
-            end
-          end
-        end
-      end
-      
       distance_count <= distance_count + 1;
       if (distance_count == 16'b0000001111100110)
         state <= state2;
@@ -79,11 +51,6 @@ case (state)
   state2:
   begin
     distance_count <= 0;
-    sec_0 <= 0;
-    sec_1 <= 0;
-    sec_2 <= 0;
-    sec_3 <= 0;
-
     distance <= distance_count;
     distance_display = {sec_3,sec_2,sec_1,sec_0};
     state <= state0;
@@ -92,6 +59,5 @@ case (state)
 endcase
 
 assign text = distance;
-assign text_out = distance_display;
 
 endmodule
--- Sonic.srcs/sources_1/new/Top.v
+++ Sonic.srcs/sources_1/new/Top.v
@@ -19,24 +19,20 @@ wire [15:0] text1;
 wire [15:0] text2;
 wire [15:0] text3;
 
-wire [15:0] text1_out;
-wire [15:0] text2_out;
-wire [15:0] text3_out;
-
 wire [3:0] type;
 
 Sonic_trig prog1(clk, trig[0]);
-Sonic_echo prog2(clk, echo[0], text1,text1_out);
+Sonic_echo prog2(clk, echo[0], text1);
 Sonic_trig prog3(clk, trig[1]);
-Sonic_echo prog4(clk, echo[1], text2,text2_out);
+Sonic_echo prog4(clk, echo[1], text2);
 Sonic_trig prog5(clk, trig[2]);
-Sonic_echo prog6(clk, echo[2], text3,text3_out);
+Sonic_echo prog6(clk, echo[2], text3);
 PWM prog7(clk,type[0],motor1);
 PWM prog8(clk,type[1],motor2);
 PWM prog9(clk,type[2],motor3);
 PWM prog10(clk,type[3],motor4);
 Control prog11(text1,text2,text3,type);
-led_seg prog12(clk, text2_out, text3_out, post1, post2, seg1, seg2);
+led_seg prog12(clk, text2, text3, post1, post2, seg1, seg2);
 led_display prog13(type, led);
 
 
